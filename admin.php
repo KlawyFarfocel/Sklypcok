263f2fc2513d49c70cf41f4c9b02929002ec915a
Klawy Farfocel <smierciak.maciej@gmail.com>
2020-03-12 13:11:50 +0100

    dorobić rozmiary md w adminie

---
+++ admin.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/admin.css">
+    <title>Hello, world!</title>
+  </head>
+<body>
+<?php
+        $connect=new mysqli('localhost','root','','projektms');
+        session_start();
+?>
+<div class="wrap">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="#" class="mx-auto navbar-brand d-xs-block d-lg-none">Navbar</a>
+        <a class="navbar-brand d-none d-lg-block mx-auto" href="index.php">Navbar</a>
+    </nav>
+    <?php
+        if(isset($_SESSION['admin'])){
+            if($_SESSION['admin']==1){
+            echo<<<alias
+                <div class="container-fluid">
+                    <div class="row row-top mt-lg-5">
+                        <div class="col-lg-4 col-6"><div class="box ml-lg-5"></div></div>
+                        <div class="col-lg-4 col-6"><div class="box ml-lg-1"></div></div>
+                    </div>
+                    <div class="row mt-lg-5">
+                        <div class="col-lg-4 col-6"><div class="box ml-lg-5"></div></div>
+                        <div class="col-lg-4 col-6"><div class="box ml-lg-1"></div></div>
+                    </div>
+                </div>
+            alias;
+            }
+            else{
+                echo<<<alias
+                <script> window.location.href="index.php" </script>
+                alias;
+            }
+        }
+        else{
+            echo<<<alias
+            <script> window.location.href="index.php" </script>
+            alias;
+        }
+     ?>
+    </div> <!-- koniec diva wrap -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6d0c8b4bd0.js" crossorigin="anonymous"></script>
+</body>
+</html>
